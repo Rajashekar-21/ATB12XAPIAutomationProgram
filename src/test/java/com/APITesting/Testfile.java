f05cd27f99906654aa77ab28496c0439de733dfb
RAj <rrajshekar318@gmail.com>
2025-09-14 17:32:18 +0530

    1st commit of API testing..

--- src/test/java/com/APITesting/Testfile.java
+++ src/test/java/com/APITesting/Testfile.java
@@ -2,6 +2,6 @@ package com.APITesting;
 
 public class Testfile {
     public static void main(String[] args) {
-        System.out.println("Hello API");
+        System.out.println("Hello API test");
     }
 }
